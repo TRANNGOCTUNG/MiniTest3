76d195d4400ba80d98a84dc7b69c3ab3b12914cc
FiveWing96 <117134067+FiveWing96@users.noreply.github.com>
2023-01-10 00:58:10 +0700

    data

--- test1.java
+++ test1.java
@@ -1,37 +1,33 @@
+import com.sun.security.jgss.GSSUtil;
+
 import java.util.Scanner;
 
 public class test1 {
-    public static int prime(int num) {
-        boolean checkPrime ;
-        if (num == 2) {
-            System.out.println(num + "is prime ");
-        } else {
-            System.out.println("Prime numbers are: ");
-            System.out.println(2);
-            for (int i = 3; i < num; i++) {
-                checkPrime = true;
-                for (int j = 2; j < i; j++) {
-                    if (i % j == 0) {
-                        checkPrime = false;
-                        break;
-                    }
-                }
-                if (checkPrime) ;
-                System.out.println(i);
-            }
 
-
+    public static boolean checkPrime(int num) {
+        if (num < 2) {
+            return false;
         }
-        return num;
+        for (int j = 2; j < num; j++) {
+            if (num % j == 0) {
+                return false;
+            }
+        }
+        return true;
     }
-
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a prime");
         int numbers;
         numbers = scanner.nextInt();
-        prime(numbers);
-
+        System.out.println("Prime is : ");
+        int n = 2;
+        while (n < numbers) {
+            if (checkPrime(n)) {
+                System.out.println(n);
+            }
+            n++;
+        }
     }
 }
